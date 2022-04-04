2c94c22b7f77b3538eea6302c83a6b2fa3cc9ddc
zhenxu66 <zhenxu66@gmail.com>
2022-04-04 08:20:18 -0700

    update

--- asyncio_thread/async_demo_2.py
+++ asyncio_thread/async_demo_2.py
@@ -3,11 +3,11 @@ import asyncio
 
 async def fetch_data():
     print("start fetching")
-    
+
 
 async def main():
     task1 = asyncio.create_task(fetch_data())
 
-
+    
 if __name__ == "__main__":
     asyncio.run(main())
